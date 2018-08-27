306ff6c80930814356db98ca85c128d071f4cf4e
djmorosini <djmorosini@outlook.com>
2018-08-27 16:12:47 -0400

    added basic css in index.css file

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './index.css'
 
 class App extends Component {
   render() {
     return (
+      <div>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+
+        <p className="App-title">Welcome to React</p>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit and save to reload.
         </p>
+      </div>
+      <div className="App">
+
+        <p className="App-title">Welcome to React</p>
+        <p className="App-intro">
+          To get started, edit and save to reload.
+</p>
+      </div>
       </div>
     );
   }
