b47eefd2e1da315db4a27a04dd801fd9886b43f0
djmorosini <djmorosini@outlook.com>
2018-08-28 17:01:18 -0400

    added urls next to titles

--- src/StoryCard.js
+++ src/StoryCard.js
@@ -32,18 +32,18 @@ class StoryCard extends Component {
 
     timeAdjust = () => {
         let timeCreated = this.state.story.time * 1000
-        let timeSince = new Date() - timeCreated 
+        let timeSince = new Date() - timeCreated
         let minutes = ((timeSince / 1000) / 60)
         console.log(timeCreated)
         console.log(minutes)
-        if (minutes < 1){
+        if (minutes < 1) {
             return ' just now '
         } else if (minutes < 60) {
             return Math.floor(minutes) + ' minutes ago '
-        }else if (minutes >= 60) {
+        } else if (minutes >= 60) {
             let hours = minutes / 60
             let minutesRemaining = minutes % 60
-            return Math.floor(hours) + ' hours ' +  (Math.floor(minutesRemaining) !== 0? Math.floor(minutesRemaining) + ' minutes ago': "ago")
+            return Math.floor(hours) + ' hours ' + (Math.floor(minutesRemaining) !== 0 ? Math.floor(minutesRemaining) + ' minutes ago' : "ago")
         }
     }
 
@@ -55,29 +55,40 @@ class StoryCard extends Component {
                 <div className='story-cards'>Loading...</div>
             )
         } else {
+            console.log(typeof story.url)
             return (
                 <li className="story-cards" key={this.props.storyId}>
-                    <a href={story.url}><span className='title'>
-                    {story.title}
-                    </span> 
-                    </a>
+                    {(story.url ?
+                        <a href={story.url}><span className='title'>
+                            {story.title}
+                        </span>
+                            <span className='url'>
+                                ({story.url.slice(0, 25) + '...'})
+                    </span>
+                        </a> :
+                        <Link to={`/comments/${this.props.storyId}`} param={{ storyid: this.props.storyId }}>
+                            <span className='title'>
+                                {story.title}
+                            </span>
+                        </Link>
+                    )}
                     <span className='time-since'>
                         {' Posted: ' + this.timeAdjust()}
                     </span>
-                    <br/>
+                    <br />
                     <span className='score'>
-                    {' ' + story.score + ' points' }
-                    </span> 
-                    <span className='author'>
-                         {' by ' + story.by + ' | '}
-                    </span> 
-                    <Link to={`/comments/${this.props.storyId}`} param={{ storyid : this.props.storyId}}>
-                    <span className='comment-number'>
-                        {story.descendants === 0? 'discuss' : story.descendants + ' comments'}
+                        {' ' + story.score + ' points'}
                     </span>
+                    <span className='author'>
+                        {' by ' + story.by + ' | '}
+                    </span>
+                    <Link to={`/comments/${this.props.storyId}`} param={{ storyid: this.props.storyId }}>
+                        <span className='comment-number'>
+                            {story.descendants === 0 ? 'discuss' : story.descendants + ' comments'}
+                        </span>
                     </Link>
-                    </li>
-                    
+                </li>
+
             )
         }
     }
